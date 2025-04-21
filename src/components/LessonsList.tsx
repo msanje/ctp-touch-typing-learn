b6446b8b861c9e07bc282195bedfc3bef810e4a4
M-Sanjay12o52o <msnjy12o52o@gmail.com>
2025-04-21 21:19:31 +0530

    next/Image to next/image

--- src/components/LessonsList.tsx
+++ src/components/LessonsList.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import Image from "next/Image";
+import Image from "next/image";
 import { Book, CheckCircle, Circle } from "lucide-react";
 import {
   ExerciseProgress,
